0118187a48af572fae7c7379628ae582a41ade73
ahmed <ahmed.benlebna@gmail.com>
2025-11-13 15:41:24 +0100

    add cursor pointer

--- components/ButtonGroup.tsx
+++ components/ButtonGroup.tsx
@@ -5,14 +5,14 @@ export default function ButtonGroup() {
     return (
         <div className="flex w-full max-w-sm flex-col gap-3 px-4 py-3">
             <Link href={'/play'}>
-                <button className="h-12 w-full rounded-lg bg-primary px-5 text-base font-bold text-white transition-transform duration-200 hover:scale-105">
+                <button className="h-12 w-full rounded-lg bg-primary px-5 text-base font-bold text-white transition-transform duration-200 hover:scale-105 cursor-pointer">
                     Play Now
                 </button>
             </Link>
 
             <Link href={'/didacticiel'}>
                 <button
-                    className="h-12 w-full rounded-lg bg-primary/30 px-5 text-base font-bold text-white transition-transform duration-200 hover:scale-105">
+                    className="h-12 w-full rounded-lg bg-primary/30 px-5 text-base font-bold text-white transition-transform duration-200 hover:scale-105 cursor-pointer">
                     How to Play
                 </button>
             </Link>
